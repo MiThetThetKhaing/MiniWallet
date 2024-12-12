6baf65f3ff9e964abf772b04765225c509984d60
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-12 14:26:40 +0630

    feat: update program

--- MiniWallet/Program.cs
+++ MiniWallet/Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using MiniWallet.Database.Models;
+using MiniWallet.Domain.Featuers.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("MainDbConnection"));
-});
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
 // Add services to the container.
+builder.Services.AddScoped<DepositWithdrawServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
