ed1b664849a9cb80d68be7a303e74410c8a1c99c
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-12 14:27:04 +0630

    feat: add response models

---
+++ MiniWallet.Domain/Models/DepositWithdrawResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using MiniWallet.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWallet.Domain.Models
+{
+    public class DepositWithdrawResponseModel
+    {
+        public TblDepositWithdraw TblDepositWithdraw { get; set; }
+
+        public List<TblDepositWithdraw> TblDepositWithdrawList { get; set; }
+    }
+}
