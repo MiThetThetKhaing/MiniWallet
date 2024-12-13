40e358a193a9b73e12efadc28753cfd1bdac615c
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-13 11:14:10 +0630

    feat: update request model

---
+++ MiniWallet.Domain/Models/DepositWithdrawRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWallet.Domain.Models
+{
+    public class DepositWithdrawRequestModel
+    {
+        public int Id { get; set; }
+        public string TxnNo { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime TxnDate { get; set; }
+        public decimal Amount { get; set; }
+        public string TransactionType { get; set; }
+    }
+}
