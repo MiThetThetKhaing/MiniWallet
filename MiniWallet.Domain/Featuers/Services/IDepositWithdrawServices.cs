ae4845e9fa9c1e411f11f539534e1e17d15d4083
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-13 11:13:33 +0630

    fix: update Iservice

--- MiniWallet.Domain/Featuers/Services/IDepositWithdrawServices.cs
+++ MiniWallet.Domain/Featuers/Services/IDepositWithdrawServices.cs
@@ -1,12 +1,11 @@
-﻿using MiniWallet.Database.Models;
-using MiniWallet.Domain.Models;
+﻿using MiniWallet.Domain.Models;
 
 namespace MiniWallet.Domain.Featuers.Services
 {
     public interface IDepositWithdrawServices
     {
-        Task<Result<DepositWithdrawResponseModel>> CreateDeposit(TblDepositWithdraw deposit);
-        Task<Result<DepositWithdrawResponseModel>> CreateWithdraw(TblDepositWithdraw withdraw);
+        Task<Result<DepositWithdrawResponseModel>> CreateDeposit(string mobileNo, decimal amount);
+        Task<Result<DepositWithdrawResponseModel>> CreateWithdraw(string mobileNo, decimal amount);
         Task<Result<DepositWithdrawResponseModel>> GetAllDepositWithdraw(string mobileNo);
     }
 }
