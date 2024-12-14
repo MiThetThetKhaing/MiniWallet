1cb0730637677d3fb673c619afb7822e0ac22835
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-14 19:34:21 +0630

    feat: try catch in wallet API

--- MiniWallet/Controllers/Endpoints/WalletController.cs
+++ MiniWallet/Controllers/Endpoints/WalletController.cs
@@ -19,36 +19,70 @@ namespace MiniWallet.Api.Controllers.Endpoints
         [HttpGet("{mobileNo}")]
         public async Task<IActionResult> GetWallet(string mobileNo)
         {
-            var response = await _service.GetWallet(mobileNo);
-            return Execute(response);
+            try
+            {
+                var response = await _service.GetWallet(mobileNo);
+                return Execute(response);
+            }
+            catch (Exception ex) {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost()]
         public async Task<IActionResult> CreateWallet(WalletRequestModel newWallet)
         {
-            var response = await _service.CreateWallet(newWallet);
-            return Execute(response);
+            try
+            {
+                var response = await _service.CreateWallet(newWallet);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPatch("{mobileNo}")]
         public async Task<IActionResult> ChangePin(string mobileNo, int oldPin, int newPin)
         {
-            var response = await _service.ChangePin(mobileNo, oldPin, newPin);
-            return Execute(response);
+            try
+            {
+                var response = await _service.ChangePin(mobileNo, oldPin, newPin);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPatch("")]
         public async Task<IActionResult> ChangeMobileNo(string oldMobileNo, string newMobileNo, int pin)
         {
-            var response = await _service.ChangeMobileNo(oldMobileNo, newMobileNo, pin);
-            return Execute(response);
+            try
+            {
+                var response = await _service.ChangeMobileNo(oldMobileNo, newMobileNo, pin);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPatch("{mobileNo}")]
         public async Task<IActionResult> UpdateNames(string mobileNo, int pin, string? userName, string? fullName)
         {
-            var response = await _service.ChangeNames(mobileNo, pin, userName, fullName);
-            return Execute(response);
+            try
+            {
+                var response = await _service.ChangeNames(mobileNo, pin, userName, fullName);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
